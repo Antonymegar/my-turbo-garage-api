4371d85102a2380223d305d15c3844c32ea7e3e0
Antonymegar <antonymurugu@outlook.com>
2025-02-04 14:54:46 +0300

    Enabling gorm with aivencloud

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ import (
 )
 
 func main() {
+	serviceURI := "postgres://avnadmin:AVNS_7WwKRw8p2iNx6LqPmag@pg-17b5fac5-kithinjimurugu-bf72.e.aivencloud.com:10566/defaultdb?sslmode=require"
 
-	conn, _ := url.Parse()
+	conn, _ := url.Parse(serviceURI)
 	conn.RawQuery = "sslmode=verify-ca;sslrootcert=ca.pem"
 
 	db, err := sql.Open("postgres", conn.String())
